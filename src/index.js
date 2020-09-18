140645d4b044f7b362f259b136c84c2f0156cfa9
Angie Cruz <acruz@dchanges.com>
2020-09-18 18:21:32 -0500

    Se instala vercel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hola! :D</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Ta madre:D</h1>, document.getElementById('app'))
