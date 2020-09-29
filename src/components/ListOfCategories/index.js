f477f60be50022c7a5c3d5a4ccab5a0b9edb45be
Angie Cruz <16angie.c@gmail.com>
2020-09-28 22:33:56 -0500

    Se cambia la informacion quemada por informacion real de las categorias traida desde la api con Operator.Rest

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { categories } from '../../../api/db.json';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
@@ -6,7 +7,7 @@ export const ListOfCategories = () => {
     return (
         <List>
             {
-                [1, 2, 3, 4].map(category => <Item key={category}> <Category /> </Item>)
+                categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
             }
         </List>
     )
