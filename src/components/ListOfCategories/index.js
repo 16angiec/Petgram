7fd344c31d32ca873843fa6726f87bc3a9e10f76
Angie Cruz <16angie.c@gmail.com>
2020-09-29 23:45:44 -0500

    Se agrega useState y useEffect para hacer la lista de categorias fija

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { categories } from '../../../api/db.json';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
-    return (
-        <List>
+    const [categories, setCategories] = useState([])
+    const [showFixed, setShowFixed] = useState(false)
+
+    useEffect(function () {
+        window.fetch('https://petgram-api-test-rmx6dg1z5.vercel.app/categories')
+            .then(res => res.json())
+            .then(response => {
+                setCategories(response)
+            })
+    }, [])
+
+    useEffect(function () {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+
+        document.addEventListener('scroll', onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''} >
             {
                 categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
             }
         </List>
+    )
+
+    return (
+        <Fragment>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </Fragment>
     )
 }
