98d47cea75ae34aeb7c66474b13654dfbc0e8fb4
Angie Cruz <acruz@dchanges.com>
2020-10-01 16:42:13 -0500

    Se usa render props para recuperar una foto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo/index';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './container/ListOfPhotoCards';
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
 
-export const App = () => (
-    <>
-        <GlobalStyle />
-        <Logo />
-        <ListOfCategories />
-        <ListOfPhotoCards categoryId={1} />
-    </>
-)
+export const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    return (
+        <>
+            <GlobalStyle />
+            <Logo />
+            {
+                detailId
+                    ? <PhotoCardWithQuery id ={detailId} />
+                    : <Fragment>
+                        <ListOfCategories />
+                        <ListOfPhotoCards categoryId={1} />
+                    </Fragment>
+            }
+
+        </>
+    )
+}
