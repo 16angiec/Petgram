74a0e32d9c9631d4ba6c3c2278abd3a95ef707ff
Angie Cruz <acruz@dchanges.com>
2020-09-30 19:02:51 -0500

    Se agrega polyfill de Intersection Observer e imports dinamicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
-import { Article,ImgWrapper, Img, Button } from './styles';
+import { Article, ImgWrapper, Img, Button } from './styles';
 import { MdFavoriteBorder } from "react-icons/md";
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
@@ -8,15 +8,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     const [show, setShow] = useState(false)
 
     useEffect(function () {
-        const observer = new window.IntersectionObserver(function (entries) {
-            const { isIntersecting } = entries[0];
-            if (isIntersecting) {
-                console.log('si')
-                setShow(true)
-                observer.disconnect()
-            }
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then(() => {
+            const observer = new window.IntersectionObserver(function (entries) {
+                const { isIntersecting } = entries[0];
+                if (isIntersecting) {
+                    setShow(true)
+                    observer.disconnect()
+                }
+            })
+            observer.observe(ref.current)
         })
-        observer.observe(ref.current)
+
     }, [ref])
 
     return (
