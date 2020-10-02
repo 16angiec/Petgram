479f4f820d8d62a96710c7dca92db9ad404ac006
Angie Cruz <acruz@dchanges.com>
2020-10-02 15:36:47 -0500

    Se crea la pagina Detail

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo/index';
 import { Home } from './Pages/Home';
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
+import { Detail } from './Pages/Detail';
 
 export const App = () => {
-    const urlParams = new window.URLSearchParams(window.location.search)
-    const detailId = urlParams.get('detail')
     return (
         <>
             <GlobalStyle />
             <Logo />
-            {
-                detailId
-                    ? <PhotoCardWithQuery id={detailId} />
-                    : <Router>
-                        <Home path='/' />
-                        <Home path='/pet/:id' />
-                    </Router>
-            }
-
+            <Router>
+                <Home path='/' />
+                <Home path='/pet/:id' />
+                <Detail path='/detail/:detailId' />
+            </Router>
         </>
     )
 }
