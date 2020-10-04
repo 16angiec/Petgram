2136d3c08511d2ea44490bb50bdf6486e57a8433
Angie Cruz <16angie.c@gmail.com>
2020-10-04 18:45:47 -0500

    Controla el estado de carga y error al registrar un usuario

--- src/Pages/NotRegisteredUser.js
+++ src/Pages/NotRegisteredUser.js
@@ -11,14 +11,17 @@ export const NotRegisteredUser = () => (
                     <Fragment>
                         <RegisterMutation>
                             {
-                                (register) => {
+                                (register, { data, loading, error }) => {
                                     const onSubmit = ({ email, password }) => {
                                         const input = { email, password }
                                         const variables = { input }
                                         register({ variables }).then(activateAuth)
                                     }
+
+                                    const errorMsg = error && 'El usuario ya existe o hay algun problema.'
+
                                     return (
-                                        <UserForm title='Registrarse' onSubmit={onSubmit} />
+                                        <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
                                     )
                                 }
                             }
